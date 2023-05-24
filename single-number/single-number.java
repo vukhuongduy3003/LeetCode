a10d504330a3d714cab4659a02a86c31fd978d8c
vukhuongduy3003 <89400058+vukhuongduy3003@users.noreply.github.com>
2023-05-24 10:56:32 +0700

    Time: 6 ms, Memory: 44.9 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,15 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++)
-            if (!map.containsKey(nums[i]))
-                map.put(nums[i], 1);
-            else
-                map.put(nums[i], map.get(nums[i]) + 1);
-        for (Integer i : map.keySet())
-            if (map.get(i) == 1)
-                return i;
-        return 0;
+        // int ans = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++)
+        //     if (!map.containsKey(nums[i]))
+        //         map.put(nums[i], 1);
+        //     else
+        //         map.put(nums[i], map.get(nums[i]) + 1);
+        // for (Integer i : map.keySet())
+        //     if (map.get(i) == 1)
+        //         return i;
+        // return 0;
+        
+        // Different ways
+        int n = nums.length;
+        Arrays.sort(nums);
+        if (n == 1) return nums[0];
+        for (int i = 0; i < n - 1; i += 2)
+            if (nums[i] != nums[i + 1]) return nums[i];
+        return nums[n - 1];
     }
 }
