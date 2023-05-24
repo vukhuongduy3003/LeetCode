87e5329eac7c6f2260395530c22686a5cd06c1e9
vukhuongduy3003 <89400058+vukhuongduy3003@users.noreply.github.com>
2023-05-24 12:16:08 +0700

    Time: 1 ms, Memory: 44.6 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -13,11 +13,17 @@ class Solution {
         // return 0;
         
         // Different ways
-        int n = nums.length;
-        Arrays.sort(nums);
-        if (n == 1) return nums[0];
-        for (int i = 0; i < n - 1; i += 2)
-            if (nums[i] != nums[i + 1]) return nums[i];
-        return nums[n - 1];
+        // int n = nums.length;
+        // Arrays.sort(nums);
+        // if (n == 1) return nums[0];
+        // for (int i = 0; i < n - 1; i += 2)
+        //     if (nums[i] != nums[i + 1]) return nums[i];
+        // return nums[n - 1];
+        
+        // Different ways: bit manipulation
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++)
+            ans = ans ^ nums[i];
+        return ans;
     }
 }
