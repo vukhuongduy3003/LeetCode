8eff2e22bbfec9370d5f9068b2ee7a906553dcd6
vukhuongduy3003 <89400058+vukhuongduy3003@users.noreply.github.com>
2023-05-31 17:10:52 +0700

    Time: 18 ms, Memory: 44.4 MB - LeetHub

--- valid-sudoku/valid-sudoku.java
+++ valid-sudoku/valid-sudoku.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> set = new HashSet<>();
-        for (int i = 0; i < 9; i++)
-            for (int j = 0; j < 9; j++){
-                if (board[i][j] != '.'){
-                    char num = board[i][j];
-                    if (!set.add(num + " at row " + i) ||
-                        !set.add(num + " at column " + j) ||
-                        !set.add(num + " at grid " + i / 3 + " - " + j / 3)){
-                        return false;
+        int size = board.length;
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++){
+                Character c = board[i][j];
+                if (c != '.')
+                    if (!set.add(c + " at row " + i) ||
+                        !set.add(c + " at col " + j) ||
+                        !set.add(c + " at grid " + i / 3 + j / 3)){
+                            return false;
                     }
-                }
             }
         return true;
     }
