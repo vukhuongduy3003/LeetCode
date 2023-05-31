97355f4bb5c637fc5c440f9a42379a9e6f4a1cec
vukhuongduy3003 <89400058+vukhuongduy3003@users.noreply.github.com>
2023-05-31 16:51:43 +0700

    Time: 1 ms, Memory: 45.8 MB - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,29 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // My Ways
-        // int n = nums.length;
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // int count = 0;
-        // for (int i = 0; i < n; i++){
-        //     if (nums[i] != 0){
-        //         map.put(count++, nums[i]);
-        //     }
-        // }
-        // int[] nums1 = new int[n];
-        // for (Integer i : map.keySet()){
-        //     nums1[i] = map.get(i);
-        // }
-        // System.arraycopy(nums1, 0, nums, 0, n);
-        
-        // Different Way
         int snowBallSize = 0;
         for (int i = 0; i < nums.length; i++){
             if (nums[i] == 0){
                 snowBallSize++;
             }
             else if (snowBallSize > 0){
-                nums[i-snowBallSize]=nums[i];
-                nums[i]=0;
+                nums[i - snowBallSize] = nums[i];
+                nums[i] = 0;
             }
         }
     }
