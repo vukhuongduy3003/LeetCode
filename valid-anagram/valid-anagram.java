dd61712eb6776cda3cc8a8acfbd1330c1394982d
vukhuongduy3003 <89400058+vukhuongduy3003@users.noreply.github.com>
2023-06-01 08:54:31 +0700

    Time: 23 ms, Memory: 42.2 MB - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,14 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // my way
         if (s.length() != t.length()) return false;
-        int[] arr = new int[26];
+        HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++){
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+            Character s1 = s.charAt(i);
+            Character t1 = t.charAt(i);
+            map.put(s1, map.getOrDefault(s1, 0) + 1);
+            map.put(t1, map.getOrDefault(t1, 0) - 1);
         }
-        for (int i = 0; i < 26; i++){
-            if (arr[i] != 0) return false;
+        for (Integer i : map.values()){
+            if (i != 0) return false;
         }
         return true;
+        // Different way
+        // if (s.length() != t.length()) return false;
+        // int[] arr = new int[26];
+        // for (int i = 0; i < s.length(); i++){
+        //     arr[s.charAt(i) - 'a']++;
+        //     arr[t.charAt(i) - 'a']--;
+        // }
+        // for (int i = 0; i < 26; i++){
+        //     if (arr[i] != 0) return false;
+        // }
+        // return true;
     }
 }
