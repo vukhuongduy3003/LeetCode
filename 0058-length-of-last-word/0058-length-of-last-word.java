98d62a50b2b9f21d2dbeb9e2e09038b6a56351ea
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-05-26 10:01:31 +0700

    Time: 0 ms (100.00%), Space: 41.6 MB (55.85%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1, length = 0;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        return length;
+    }
+}
