7982fde7c9ff6c5ae75d81da40150fc7c0c808f0
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-06-04 10:14:33 +0700

    Time: 4 ms (69.65%), Space: 42.2 MB (12.05%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,23 +1,15 @@
 class Solution {
     public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
         int ans = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
         for (Character c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
-                if (map.get(c) % 2 == 0) {
-                    ans += 2;
-                }
+            if (set.contains(c)) {
+                set.remove(c);
+                ans += 2;
             } else {
-                map.put(c, 1);
+                set.add(c);
             }
         }
-        for (Map.Entry<Character, Integer> e : map.entrySet()) {
-            if (e.getValue() % 2 == 1) {
-                ans += 1;
-                break;
-            }
-        }
-        return ans;
+        return set.size() > 0 ? ans + 1 : ans;
     }
 }
