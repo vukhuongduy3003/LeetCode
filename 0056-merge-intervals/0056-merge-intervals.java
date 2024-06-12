0a1554700cffb8a8722e6661b6b47595c099188a
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-06-12 17:07:51 +0700

    Time: 8 ms (86.02%), Space: 46.4 MB (61.39%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -4,15 +4,15 @@ class Solution {
         int n = intervals.length;
         int[][] ans = new int[n][2];
         ans[0] = intervals[0];
-        int j = 1;
+        int j = 0;
         for (int i = 1; i < n; i++) {
-            if (ans[j - 1][1] >= intervals[i][0]) {
-                ans[j - 1][1] = Math.max(ans[j - 1][1], intervals[i][1]);
+            if (ans[j][1] >= intervals[i][0]) {
+                ans[j][1] = Math.max(ans[j][1], intervals[i][1]);
             } else {
                 j++;
-                ans[j - 1] = intervals[i];
+                ans[j] = intervals[i];
             }
         }
-        return Arrays.copyOf(ans, j);
+        return Arrays.copyOf(ans, j + 1);
     }
 }
