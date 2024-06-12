7645ce2ea015f88ab6a9c730b10ec9ad21f705f3
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-06-12 21:41:12 +0700

    Time: 17 ms (7.78%), Space: 45.4 MB (5.61%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.java
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -1,10 +1,23 @@
 class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
-        int ans = 0;
-        for (int i = 0; i < timeSeries.length - 1; i++) {
-            ans += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+        int n = timeSeries.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new int[]{timeSeries[i], timeSeries[i] + duration - 1};
         }
-        ans += duration;
+        List<Integer[]> list = new ArrayList<Integer[]>();
+        list.add(new Integer[]{arr[0][0], arr[0][1]});
+        for (int i = 1; i < n; i++) {
+            if (list.get(list.size() - 1)[1] >= arr[i][0]) {
+                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], arr[i][1]);
+            } else {
+                list.add(new Integer[]{arr[i][0], arr[i][1]});
+            }
+        }
+        int ans = 0;
+        for (Integer[] e : list) {
+            ans += e[1] - e[0] + 1;
+        }
         return ans;
     }
 }
