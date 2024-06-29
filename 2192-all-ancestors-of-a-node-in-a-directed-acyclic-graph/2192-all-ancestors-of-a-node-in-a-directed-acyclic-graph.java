2e6cadfe2a15efd7073019fe56658a48adfb35cc
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-06-30 05:33:38 +0700

    Time: 58 ms (83.91%), Space: 65.4 MB (59.44%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,37 +1,30 @@
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<List<Integer>> arr = new ArrayList<>();
-        
-        // Initialize adjacency list and visited list
+        List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            arr.add(new ArrayList<>());
+            ans.add(new ArrayList<>());
+            graph[i] = new ArrayList<>();
         }
-        
-        // Populate the adjacency list
         for (int[] e : edges) {
-            int u = e[0];
-            int v = e[1];
-            arr.get(v).add(u);
+            int u = e[0], v = e[1];
+            graph[u].add(v);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(graph, ans, new boolean[n], i, i);
+        }
+        for (int i = 0; i < n; i++) {
+            ans.get(i).sort(Integer::compareTo);
         }
         
-        // Find ancestors for each node
-        for (int i = 0; i < n; i++) {
-            List<Integer> temp = new ArrayList<>();
-            List<Boolean> visited = new ArrayList<>(Collections.nCopies(n, false));
-            dfs(i, arr, temp, visited);
-            Collections.sort(temp);
-            ans.add(temp);
-        }
         return ans;
     }
-    
-    private void dfs(int u, List<List<Integer>> arr, List<Integer> temp, List<Boolean> visited) {
-        visited.set(u, true);
-        for (int v : arr.get(u)) {
-            if (!visited.get(v)) {
-                temp.add(v);
-                dfs(v, arr, temp, visited);
+    private void dfs(List<Integer>[] graph, List<List<Integer>> ans, boolean[] visited, int parent, int curr) {
+        visited[curr] = true;
+        for (int dest : graph[curr]) {
+            if (!visited[dest]) {
+                ans.get(dest).add(parent);
+                dfs(graph, ans, visited, parent, dest);
             }
         }
     }
