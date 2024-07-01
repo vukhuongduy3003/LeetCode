394be0ddd8deb7130abee7767bc852eed0c549b1
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-07-01 12:45:39 +0700

    Time: 2 ms (85.94%), Space: 45.5 MB (31.01%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,37 @@
+class Solution {
+    private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length || board[r][c] != 'O') {
+            return;
+        }
+        board[r][c] = 'T';
+        dfs(board, r - 1, c);
+        dfs(board, r + 1, c);
+        dfs(board, r, c - 1);
+        dfs(board, r, c + 1);
+    }
+    public void solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
+                    dfs(board, i, j);
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+}
