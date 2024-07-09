3b82c4264930de23c59223c6298573f1df2c3636
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-07-09 10:47:20 +0700

    Time: 3 ms (93.06%), Space: 72.9 MB (32.65%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        double sumTime = 0.0;
+        int time = 0;
+        for (int[] c : customers) {
+            time = (time < c[0])? c[0] : time;
+            time += c[1];
+            sumTime += time - c[0];
+        }
+        return sumTime / customers.length;
+    }
+}
