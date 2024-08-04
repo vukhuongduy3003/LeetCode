e143d0981b2db794b3d5f290f769065303f3422c
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-08-04 21:52:26 +0700

    Time: 189 ms (15.31%), Space: 69.7 MB (29.73%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        final int MOD = 1000000007;
+        
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                minHeap.offer(prefix[i] - prefix[j]);
+            }
+        }
+        
+        long result = 0;
+        for (int i = 1; i <= right; i++) {
+            int sum_value = minHeap.poll();
+            if (i >= left) {
+                result = (result + sum_value) % MOD;
+            }
+        }
+        
+        return (int) result;
+    }
+}
