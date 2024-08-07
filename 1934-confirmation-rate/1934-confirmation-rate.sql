a423241c7f34921792c4e871ecbadf4101ac3c31
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-08-07 11:18:23 +0700

    Time: 545 ms (44.11%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+SELECT s.user_id, ROUND(CAST(COUNT(CASE WHEN action = 'confirmed' THEN 1 ELSE NULL END) AS FLOAT) / COUNT(*), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
