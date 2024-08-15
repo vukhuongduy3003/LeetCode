caa5a54d1403a5cc177488ebe0414fe44ab5cf46
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-08-15 11:07:20 +0700

    Time: 2 ms (95.27%), Space: 55.5 MB (83.96%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int b : bills) {
+            if (b == 5) {
+                five++;
+            } else if (b == 10) {
+                if (five > 0) {
+                    ten++;
+                    five--;
+                } else {
+                    return false;
+                }
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
