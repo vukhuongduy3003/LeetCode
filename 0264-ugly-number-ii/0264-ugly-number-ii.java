087e4d92615b8f1e58224d3ab276725458ab6506
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-08-19 10:56:42 +0700

    Time: 2 ms (92.13%), Space: 41.6 MB (55.88%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,17 +1,25 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        TreeSet<Long> uglyNumbersSet = new TreeSet<>();
-        uglyNumbersSet.add(1L);
-
-        Long currentUgly = 1L;
-        for (int i = 0; i < n; i++) {
-            currentUgly = uglyNumbersSet.pollFirst();
-
-            uglyNumbersSet.add(currentUgly * 2);
-            uglyNumbersSet.add(currentUgly * 3);
-            uglyNumbersSet.add(currentUgly * 5);
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int nextVal2 = 2, nextVal3 = 3, nextVal5 = 5;
+        for (int i = 1; i < n; i++) {
+            int nextUglyNum = Math.min(nextVal2, Math.min(nextVal3, nextVal5));
+            dp[i] = nextUglyNum;
+            if (nextUglyNum == nextVal2) {
+                index2++;
+                nextVal2 = dp[index2] * 2;
+            }
+            if (nextUglyNum == nextVal3) {
+                index3++;
+                nextVal3 = dp[index3] * 3;
+            }
+            if (nextUglyNum == nextVal5) {
+                index5++;
+                nextVal5 = dp[index5] * 5;
+            }
         }
-
-        return currentUgly.intValue();
+        return dp[n - 1];
     }
 }
