df3de3139517b13fc3821b7b9d0ca0745f0a0ae4
Vũ Khương Duy <89400058+vukhuongduy3003@users.noreply.github.com>
2024-08-20 11:27:19 +0700

    Time: 3008 ms (81.82%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,6 +1,7 @@
 /* Write your T-SQL query statement below */
-SELECT s.product_id, tb.first_year, s.quantity, s.price
-FROM (SELECT product_id, MIN(year) AS first_year FROM Sales GROUP BY product_id) tb
-JOIN Product p ON p.product_id = tb.product_id
-JOIN Sales s ON tb.product_id = s.product_id
-WHERE tb.first_year = s.year;
+WITH CTE AS (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales GROUP BY product_id
+)
+SELECT s.product_id, first_year, quantity, price
+FROM Sales s JOIN CTE ON s.product_id = CTE.product_id AND s.year = CTE.first_year;
